4cfd3ac780ab32841c505e06cdbd2f9c08d500a3
adrianhuebner <adrianh92@gmail.com>
2020-05-19 19:14:25 -0700

    New classes for Players and Monsters added, override methods added that needed

---
+++ src/com/company/Interfaces/Monsters.java
@@ -0,0 +1,21 @@
+package com.company.Interfaces;
+
+import java.util.ArrayList;
+
+public class Monsters implements ISaveable{
+
+    @Override
+    public ArrayList<String> SavedItems() {
+        return null;
+    }
+
+    @Override
+    public void PopulateFields(ArrayList<String> parameters) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Code will be here";
+    }
+}
---
+++ src/com/company/Interfaces/Players.java
@@ -0,0 +1,21 @@
+package com.company.Interfaces;
+
+import java.util.ArrayList;
+
+public class Players implements ISaveable{
+
+    @Override
+    public ArrayList<String> SavedItems() {
+        return null;
+    }
+
+    @Override
+    public void PopulateFields(ArrayList<String> parameters) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Code is going here";
+    }
+}
